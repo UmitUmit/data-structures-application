739014677cda119a81cbfb75b230cf2dbda58616
UmitUmit <umit.green@gmail.com>
2022-04-09 16:10:25 -0400

    Tree

---
+++ 06-Trees/src/main/java/MyTree.java
@@ -0,0 +1,73 @@
+public class MyTree {
+
+    public Node root;
+
+
+    public MyTree() {
+
+    }
+
+    public MyTree(Node root) {
+        this.root = root;
+    }
+
+    public void insert(int value){
+        Node newNode =  new Node(value);
+        if(root == null){
+            root = newNode;
+            return;
+        }
+        Node current = root;
+        while (true){
+            if (value <= current.value){
+                if (current.leftChild== null){
+                    current.leftChild=newNode;
+
+                    break;
+                }
+                current = current.leftChild;
+            }
+            else {
+                    if (current.rightChild== null){
+                        current.rightChild=newNode;
+
+                        break;
+                    }
+                    current = current.rightChild;
+            }
+        }
+    }// end insert
+
+
+    //PreOrder Traversal of Tree Root-left-right
+
+    public void preOrderTraversal(Node root){
+        if(root==null) return;
+        System.out.print(root.value+", ");
+        preOrderTraversal(root.leftChild);
+        preOrderTraversal(root.rightChild);
+
+    }
+
+    //InOrder Traversal of Tree Root-left-right
+
+    public void inOrderTraversal(Node root){
+        if(root==null) return;
+        inOrderTraversal(root.leftChild);
+        System.out.print(root.value+", ");
+        inOrderTraversal(root.rightChild);
+
+    }
+
+    //InOrder Traversal of Tree Root-left-right
+
+    public void postOrderTraversal(Node root){
+        if(root==null) return;
+        postOrderTraversal(root.leftChild);
+        postOrderTraversal(root.rightChild);
+        System.out.print(root.value+", ");
+
+    }
+
+
+}
