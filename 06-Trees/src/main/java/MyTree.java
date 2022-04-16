c4f12aefa1c5823ce9f1c11f4e09e4fb8007625e
UmitUmit <umit.green@gmail.com>
2022-04-16 11:29:09 -0400

    contains method added

--- 06-Trees/src/main/java/MyTree.java
+++ 06-Trees/src/main/java/MyTree.java
@@ -14,29 +14,28 @@ public class MyTree {
         this.root = root;
     }
 
-    public void insert(int value){
-        Node newNode =  new Node(value);
-        if(root == null){
+    public void insert(int value) {
+        Node newNode = new Node(value);
+        if (root == null) {
             root = newNode;
             return;
         }
         Node current = root;
-        while (true){
-            if (value <= current.value){
-                if (current.leftChild== null){
-                    current.leftChild=newNode;
+        while (true) {
+            if (value <= current.value) {
+                if (current.leftChild == null) {
+                    current.leftChild = newNode;
 
                     break;
                 }
                 current = current.leftChild;
-            }
-            else {
-                    if (current.rightChild== null){
-                        current.rightChild=newNode;
+            } else {
+                if (current.rightChild == null) {
+                    current.rightChild = newNode;
 
-                        break;
-                    }
-                    current = current.rightChild;
+                    break;
+                }
+                current = current.rightChild;
             }
         }
     }// end insert
@@ -44,9 +43,9 @@ public class MyTree {
 
     //PreOrder Traversal of Tree Root-left-right
 
-    public void preOrderTraversal(Node root){
-        if(root==null) return;
-        System.out.print(root.value+", ");
+    public void preOrderTraversal(Node root) {
+        if (root == null) return;
+        System.out.print(root.value + ", ");
         preOrderTraversal(root.leftChild);
         preOrderTraversal(root.rightChild);
 
@@ -54,37 +53,49 @@ public class MyTree {
 
     //InOrder Traversal of Tree Root-left-right
 
-    public void inOrderTraversal(Node root){
-        if(root==null) return;
+    public void inOrderTraversal(Node root) {
+        if (root == null) return;
         inOrderTraversal(root.leftChild);
-        System.out.print(root.value+", ");
+        System.out.print(root.value + ", ");
         inOrderTraversal(root.rightChild);
 
     }
 
     //InOrder Traversal of Tree Root-left-right
 
-    public void postOrderTraversal(Node root){
-        if(root==null) return;
+    public void postOrderTraversal(Node root) {
+        if (root == null) return;
         postOrderTraversal(root.leftChild);
         postOrderTraversal(root.rightChild);
-        System.out.print(root.value+", ");
+        System.out.print(root.value + ", ");
 
     }
 
 
-    public void levelOrderTraversal(Node root){
-        if (this.root ==null) return;
+    public void levelOrderTraversal(Node root) {
+        if (this.root == null) return;
         Queue<Node> queue = new LinkedList<>();
         queue.add(this.root);
-        while (!queue.isEmpty()){
+        while (!queue.isEmpty()) {
             Node toVisit = queue.poll(); //poll don't give exception
-            System.out.println(toVisit.value +", ");
-            if (toVisit.leftChild!=null) queue.add(toVisit.leftChild);
-            if (toVisit.rightChild!=null) queue.add(toVisit.rightChild);
+            System.out.println(toVisit.value + ", ");
+            if (toVisit.leftChild != null) queue.add(toVisit.leftChild);
+            if (toVisit.rightChild != null) queue.add(toVisit.rightChild);
         }// end while
 
     } //end levelOrder
 
+    //task-1 implement contains method
+    public boolean contains(int value) {
+        if (root == null) return false;
+        Node current = root;
+        while (current != null) {
+            if (value < current.value) current = current.leftChild;       //branch left
+            else if (value > current.value) current = current.rightChild;   //branch right
+            else return true;
+        }
+
+        return false;
+    }
 
 }
